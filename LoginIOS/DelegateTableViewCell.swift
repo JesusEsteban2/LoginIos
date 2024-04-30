c666764beeb79a8bc61c7e57b02eb8e504a75e5a
mananas <jesus.esteban@outlook.com>
2024-04-30 14:00:02 +0200

    1 commit 30/04/2024

---
+++ LoginIOS/DelegateTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DelegateTableViewCell.swift
+//  LoginIOS
+//
+//  Created by Mañanas on 30/4/24.
+//
+
+import UIKit
+
+class DelegateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
