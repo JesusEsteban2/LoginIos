1dd5a1d1219f11a6c3115154178c85f5a844c821
jesusesteban2 <jesus.esteban@outlook.com>
2024-05-06 22:04:38 +0200

    2 commit

--- LoginIOS/ChatViewController.swift
+++ LoginIOS/ChatViewController.swift
@@ -18,7 +18,7 @@ class ChatViewController: UIViewController,UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.dataSource=self
+        tableView.dataSource=self
         
         if (conversacionParam == nil) {
             print ("No existe conversación.")
@@ -29,36 +29,35 @@ class ChatViewController: UIViewController,UITableViewDataSource {
                 let doc=conversacionParam!.dialogId
                 let conve=conversacionParam!.conversacion
                 mensajes=try await readMensajes(doc:doc,conve:conve)
-                if !(mensajes.isEmpty) {
-                    print ("Mensaje ")
-                    tableView.reloadData()
-                } else {
+                if (mensajes.isEmpty) {
                     print ("La lista de mensajes está vacía")
                     print ("En la conversación \(conversacionParam!.dialogId)")
                 }
+                print ("Mensaje: \(mensajes[0].texto)")
+                tableView.reloadData()
             }
         }
     }
     
+    // numero de elemntos de los datos
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mensajes.count
+        mensajes.count
     }
     
+    // Render de filas
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell1:DelegateTableViewCell = tableView.dequeueReusableCell(withIdentifier: "ViewCell1", for:indexPath) as! DelegateTableViewCell
-        
-        let cell2:DelegateTableViewCell=tableView.dequeueReusableCell(withIdentifier: "ViewCell2", for: indexPath) as! DelegateTableViewCell
         
         let fila = mensajes[indexPath.row]
         
         if (fila.remitente==userIdParam) {
-            cell2.render(tipo:2,userId: fila.remitente, texto: fila.texto, fecha: fila.fecha)
+            let cell2:DelegateTableViewCell=tableView.dequeueReusableCell(withIdentifier:"ViewCell2", for: indexPath) as! DelegateTableViewCell
+            cell2.render(tipo:2,userId:fila.remitente,texto:fila.texto,fecha:fila.fecha)
             return cell2
-        } else {
-            cell1.render(tipo:1,userId: fila.remitente, texto: fila.texto, fecha: fila.fecha)
-            return cell1
         }
-      
+        
+        let cell1:DelegateTableViewCell = tableView.dequeueReusableCell(withIdentifier: "ViewCell1", for:indexPath) as! DelegateTableViewCell
+        cell1.render(tipo:1,userId: fila.remitente, texto: fila.texto, fecha: fila.fecha)
+        return cell1
     }
+      
 }
--- LoginIOS/DelegateTableViewCell.swift
+++ LoginIOS/DelegateTableViewCell.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class DelegateTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var vc1Texto: UITextView!
+
     @IBOutlet weak var vc1Fecha: UILabel!
-    @IBOutlet weak var vc2Texto: UITextView!
+    @IBOutlet weak var cv1Texto: UILabel!
     @IBOutlet weak var vc2Fecha: UILabel!
+    @IBOutlet weak var vc2Texto: UILabel!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,13 +28,16 @@ class DelegateTableViewCell: UITableViewCell {
     }
     
     func render (tipo:Int,userId:String,texto:String,fecha:Date){
+        
         if tipo==1 {
-            vc1Texto.text=texto
+            print ("celda 1")
+            cv1Texto.text=texto
             vc1Fecha.text=fecha.ISO8601Format()
-        } else {
-            vc2Texto.text=texto
-            vc2Fecha.text=fecha.ISO8601Format()
         }
+        
+        print ("celda 2")
+        vc2Texto.text=texto
+        vc2Fecha.text=fecha.ISO8601Format()
         
     }
 }
